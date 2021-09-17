b7af3f9be4973d80968dd476a8de4b8e9a1dc2f5
lokeshkumar1993 <30527740+lokeshkumar1993@users.noreply.github.com>
2021-09-17 11:48:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Popup from './Popup';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Popup />
   </React.StrictMode>,
   document.getElementById('root')
 );
